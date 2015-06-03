0c7dc86113ed69c49bc3d9b4ff5dd9c4ec6eba06
windgo <imqhf@hotmail.com>
2015-06-03 10:47:56 +0800

    create project

---
+++ QiPullMenu/QiPullMenu/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  QiPullMenu
+//
+//  Created by qi on 15/6/3.
+//  Copyright (c) 2015年 im.qihf. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
